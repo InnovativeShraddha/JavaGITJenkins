92c959f4e849adce4cf66fcf65195df238fcc6e3
InnovativeShraddha <99754844+InnovativeShraddha@users.noreply.github.com>
2022-03-16 20:31:03 +0530

    Again updated Program2.java

--- Program2.java
+++ Program2.java
@@ -3,5 +3,6 @@ class Program2{
   public static void main(system args[])
   {
     system.out.println("this is my new file");
+    system.out.println("Added new line to new file")
                        }
                        }
